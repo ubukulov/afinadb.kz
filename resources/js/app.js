9dc600cd6b653caced7e5fb9b29871727d946eed
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-11-06 11:52:29 +0600

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ require('./bootstrap');
 require('./my_scripts');
 
 window.Vue = require('vue');
-
+Vue.config.devtools = false;
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
