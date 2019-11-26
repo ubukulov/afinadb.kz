bb9c62a8963627ee5ac34dd6cdaecf4ef0d587c9
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-11-26 10:04:11 +0600

    call center leads pagination to 30

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -27,7 +27,7 @@ class Lead extends Model
                 ->select(DB::raw('leads.*, date_format(leads.tm, "%d.%m.%Y %H:%i") as dt, datediff(CURRENT_TIMESTAMP(), leads.tm) as dn, manager_leads.type AS m_type, accounts.name as user_name, accounts.last_name'))
                 ->leftJoin('manager_leads', 'manager_leads.lead_id', '=', 'leads.id')
                 ->leftJoin('accounts', 'accounts.id', '=', 'manager_leads.manager_id')
-                ->paginate(10);
+                ->paginate(30);
         }
 
         return $result;
