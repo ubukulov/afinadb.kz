a598e4c542d8b7066e3b6b2ecf2895082a9a0dca
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-11-29 18:28:25 +0600

    fix

--- resources/views/partials/sales_standarts.blade.php
+++ resources/views/partials/sales_standarts.blade.php
@@ -4,4 +4,8 @@
         <div class="bc_title">Стандарты продаж</div>
         <a href="/sources/files/Стандарты_работы.pdf" target="_blank"><div class="b_download_btn">Скачать</div></a>
     </div>
+    <div class="block_content">
+        <div class="bc_title">Анкета выявление потребности</div>
+        <a href="/sources/files/Опросник_на_казахском_языке.docx" target="_blank"><div class="b_download_btn">Скачать</div></a>
+    </div>
 </div>
