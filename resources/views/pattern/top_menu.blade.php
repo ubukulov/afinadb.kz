6c7226d63ac7fe6302a5bfdcceaf29a5d2ca80fa
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2019-12-23 11:12:01 +0600

    добавлено логотип ЦВП

--- resources/views/pattern/top_menu.blade.php
+++ resources/views/pattern/top_menu.blade.php
@@ -1,9 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="{{ route('home') }}">
         @if(Auth::user()->type == 0)
-        <img src="{{ asset('images/logo.png') }}" width="150" alt="">
+            <img src="{{ asset('images/logo.png') }}" width="150" alt="">
+        @elseif(Auth::user()->type == 2)
+            <img src="{{ asset('images/visas.png') }}" width="150" alt="">
         @else
-        <img src="{{ asset('images/257.png') }}" width="150" alt="">
+            <img src="{{ asset('images/257.png') }}" width="150" alt="">
         @endif
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
