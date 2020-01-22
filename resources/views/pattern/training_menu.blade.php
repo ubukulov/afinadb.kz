a6b0b203d9c52e674fd56c3e3005cc4ed7e0c3c5
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-01-22 13:07:09 +0600

    fx

--- resources/views/pattern/training_menu.blade.php
+++ resources/views/pattern/training_menu.blade.php
@@ -5,7 +5,7 @@
             <a class="all_education" href="#">Все обучение</a>
             <ul class="all_education_content" @if(!blockAllEducation(substr($_SERVER['REQUEST_URI'], 1))) style="display: none;" @endif>
                 <li><a class="{{ \Request::is('education*') ? 'active' : '' }}" href="{{ route('education') }}">&gt;&nbsp;Начальное обучение</a></li>
-                <li><a class="{{ \Request::is('internship-training*') ? 'active' : '' }}" href="{{ route('internship-training') }}">&gt;&nbsp;Обучение при стажировке</a></li>
+{{--                <li><a class="{{ \Request::is('internship-training*') ? 'active' : '' }}" href="{{ route('internship-training') }}">&gt;&nbsp;Обучение при стажировке</a></li>--}}
 {{--                <li><a class="{{ \Request::is('manager-training*') ? 'active' : '' }}" href="{{ route('manager-training') }}">&gt;&nbsp;Обучение для менеджера</a></li>--}}
 {{--                <li><a class="{{ \Request::is('training-from-to*') ? 'active' : '' }}" href="{{ route('training-from-to') }}">&gt;&nbsp;Обучение от ТО</a></li>--}}
 {{--                <li><a class="{{ \Request::is('private*') ? 'active' : '' }}" href="{{ route('private') }}">&gt;&nbsp;Закрытая часть</a></li>--}}
