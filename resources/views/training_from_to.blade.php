36efc75a8fc592dfae6470b4c68385cee215ad86
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-01-30 10:10:18 +0600

    fix

--- resources/views/training_from_to.blade.php
+++ resources/views/training_from_to.blade.php
@@ -36,4 +36,36 @@
             <a href="/sources/files/Занзибар презентация - Сания нов.pptx" target="_blank"><div class="b_download_btn">Скачать</div></a>
         </div>
     </div>
+
+    <div class="block">
+        <div class="block_title">Страховая Компания "Номад"</div>
+        <div class="block_content">
+            <div class="bc_title">Инструкция Амадеус</div>
+            <a href="/sources/files/Инструкция Амадеус.docx" target="_blank"><div class="b_download_btn">Скачать</div></a>
+        </div>
+        <div class="block_content">
+            <div class="bc_title">Комплексное страхование по 5 видам</div>
+            <a href="/sources/files/Комплексное страхование по 5 видам.pdf" target="_blank"><div class="b_download_btn">Скачать</div></a>
+        </div>
+        <div class="block_content">
+            <div class="bc_title">Новые правила и тарифы</div>
+            <a href="/sources/files/Новые правила и тарифы.pdf" target="_blank"><div class="b_download_btn">Скачать</div></a>
+        </div>
+        <div class="block_content">
+            <div class="bc_title">Правила ОСТ</div>
+            <a href="/sources/files/Правила ОСТ.doc" target="_blank"><div class="b_download_btn">Скачать</div></a>
+        </div>
+        <div class="block_content">
+            <div class="bc_title">Презентация  ОСТ  НОМАД Иншуранс</div>
+            <a href="/sources/files/Презентация  ОСТ  НОМАД Иншуранс.pptx" target="_blank"><div class="b_download_btn">Скачать</div></a>
+        </div>
+        <div class="block_content">
+            <div class="bc_title">Презентация НОМАД Иншуранс</div>
+            <a href="/sources/files/Презентация НОМАД Иншуранс.pptx" target="_blank"><div class="b_download_btn">Скачать</div></a>
+        </div>
+        <div class="block_content">
+            <div class="bc_title">Условия страхования МСТ</div>
+            <a href="/sources/files/Условия страхования МСТ.pdf" target="_blank"><div class="b_download_btn">Скачать</div></a>
+        </div>
+    </div>
 @stop
