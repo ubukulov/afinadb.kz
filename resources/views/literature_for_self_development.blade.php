be77a70583be6f3046196aff906600c5938bacf1
Kairat Ubukulov <kairat_ubukulov@mail.ru>
2020-07-01 11:43:44 +0600

    fix

--- resources/views/literature_for_self_development.blade.php
+++ resources/views/literature_for_self_development.blade.php
@@ -11,5 +11,13 @@
             <div class="bc_title">Максим Батырев - "45 татуировок продавана"</div>
             <div class="b_download_btn" onclick="return showVideoYoutube(this);" data-video="https://www.youtube.com/embed/h1RFdrH-ChU">Слушать</div>
         </div>
+        <div class="block_content">
+            <div class="bc_title">Максим Батырев - "45 татуировок продавана"</div>
+            <a href="/sources/books/Batyirev_M._45_Tatuirovok_Prodavana.a6.pdf" target="_blank"><div class="b_download_btn">Скачать</div></a>
+        </div>
+        <div class="block_content">
+            <div class="bc_title">Продай или продадут тебе Грант Кардон</div>
+            <div class="b_download_btn" onclick="return showVideoYoutube(this);" data-video="https://www.youtube.com/embed/HcKncnkJRjM">Слушать</div>
+        </div>
     </div>
 @stop
